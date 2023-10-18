dfdecf64e7d5869759099be017a83502f0819ba2
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-18 15:20:13 +0300

    provide currecn user to fronend

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,4 +24,10 @@ class SecurityController extends AbstractController
             'Location' => $iriConverter->getIriFromResource($user)
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached');
+    }
 }
