cd194bb84a948a61881b51ab7c998c5bb3afdca7
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 15:17:09 +0300

    provide custom logic for normalizing in decorator normalizing

--- src/Normalizer/AddOwnerGroupsNormalizer.php
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -2,18 +2,40 @@
 
 namespace App\Normalizer;
 
+use App\Entity\DragonTreasure;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
-class AddOwnerGroupsNormalizer implements NormalizerInterface
+#[AsDecorator('api_platform.jsonld.normalizer.item')]
+class AddOwnerGroupsNormalizer implements NormalizerInterface, SerializerAwareInterface
 {
+    public function __construct(
+        private readonly NormalizerInterface $normalizer,
+        private readonly Security $security
+    ) {
+    }
 
     public function normalize(mixed $object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
-        // TODO: Implement normalize() method.
+        if ($object instanceof DragonTreasure && $this->security->getUser() === $object->getOwner()) {
+            $context['groups'][] = 'owner:read';
+        }
+
+        return $this->normalizer->normalize($object, $format, $context);
     }
 
     public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
     {
-        // TODO: Implement supportsNormalization() method.
+        return $this->normalizer->supportsNormalization($data, $format, $context);
+    }
+
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        if ($this->normalizer instanceof SerializerAwareInterface) {
+            $this->normalizer->setSerializer($serializer);
+        }
     }
 }
