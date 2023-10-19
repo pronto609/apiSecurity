cd1bfa47249da46ec0906b1216803ae6b33d31dc
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 08:49:33 +0300

    test api3

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -4,14 +4,11 @@ namespace App\Tests\Functional;
 
 use App\Factory\DragonTreasureFactory;
 use App\Factory\UserFactory;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Zenstruck\Browser\Json;
-use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Browser\HttpOptions;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
-class DragonTreasureResourceTest extends KernelTestCase
+class DragonTreasureResourceTest extends ApiTestCase
 {
-    use HasBrowser;
     use ResetDatabase;
 
     public function testGetCollectionOfTreasures(): void
@@ -53,15 +50,13 @@ class DragonTreasureResourceTest extends KernelTestCase
                 'json' => []
             ])
             ->assertStatus(422)
-            ->post('/api/treasures', [
-                'json' => [
+            ->post('/api/treasures', HttpOptions::json([
                     'name' => 'A shiny thing',
                     'description' => 'Test description',
                     'value' => 1000,
                     'coolFactor' => 5,
                     'owner' => '/api/users/'. $user->getId()
-                ]
-            ])
+            ]))
             ->assertStatus(201)
             ->dump()
             ->assertJsonMatches('name', 'A shiny thing')
