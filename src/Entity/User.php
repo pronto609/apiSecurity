a8766830ea14c6fb056cf1b2598d1455b1353e2e
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 12:07:07 +0300

    add state processor which hash password before save user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -182,7 +182,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+         $this->plainPassword = null;
     }
 
     public function getUsername(): ?string
