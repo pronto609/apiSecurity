6968e8098f74e50bc1c88e6e8d49d55916c31d6d
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 14:59:53 +0300

    add own decorator normalizer for provide dinamic groups for normalizing

---
+++ src/Normalizer/AddOwnerGroupsNormalizer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class AddOwnerGroupsNormalizer implements NormalizerInterface
+{
+
+    public function normalize(mixed $object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    {
+        // TODO: Implement normalize() method.
+    }
+
+    public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool
+    {
+        // TODO: Implement supportsNormalization() method.
+    }
+}
