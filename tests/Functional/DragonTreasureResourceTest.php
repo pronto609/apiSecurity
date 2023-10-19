98752ed14844ad03341f5de95b386cdf65bb1cac
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 07:53:29 +0300

    add testing

---
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class DragonTreasureResourceTest extends KernelTestCase
+{
+    use HasBrowser;
+    use ResetDatabase;
+
+    public function testGetCollectionOfTreasures(): void
+    {
+        $this->browser()
+            ->get('/api/treasures')
+            ->dump()
+        ;
+    }
+}
