06ac97a4ec7663580987beed211cb38d24c7d85b
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-19 08:33:16 +0300

    test api2

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Functional;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Json;
 use Zenstruck\Browser\Test\HasBrowser;
@@ -35,5 +36,35 @@ class DragonTreasureResourceTest extends KernelTestCase
             "shortDescription",
             "plunderedAtAgo"
         ]);
+    }
+
+    public function testPostCreateTreasure(): void
+    {
+        $user = UserFactory::createOne();
+        $this->browser()
+//            ->post('/login', [
+//                'json' => [
+//                    'email' => $user->getEmail(),
+//                    'password' => 'pass'
+//                ]
+//            ])
+                ->actingAs($user)
+            ->post('/api/treasures', [
+                'json' => []
+            ])
+            ->assertStatus(422)
+            ->post('/api/treasures', [
+                'json' => [
+                    'name' => 'A shiny thing',
+                    'description' => 'Test description',
+                    'value' => 1000,
+                    'coolFactor' => 5,
+                    'owner' => '/api/users/'. $user->getId()
+                ]
+            ])
+            ->assertStatus(201)
+            ->dump()
+            ->assertJsonMatches('name', 'A shiny thing')
+        ;
     }
 }
